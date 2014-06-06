c3cd6e22191170a9444c8b8bc278178511252cca
Clint Checketts <clintchecketts@cchecke-ml.local>
2014-06-06 06:53:28 -0600

    Display server failures to the user

--- src/main/resources/static/js/error-app.js
+++ src/main/resources/static/js/error-app.js
@@ -40,7 +40,65 @@ app.controller('MainController', function mainController($scope,$http) {
     $scope.name = 'Bob';
 //    some.err();
 
+    $scope.hackSomething = function(){
+        $http({method:'GET',url:'/boom'})
+    }
 });
+
+
+
+
+//Http Intercpetor to check auth failures for xhr requests
+app.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('ccHttpInterceptor');
+}]);
+
+app.factory('ccHttpInterceptor', function ($q, $rootScope, $interpolate, $log, HttpLoggingService) {
+    var createLogMessage = function (response) {
+        if (response.status) {
+            return $interpolate('Http Failure: Status: {{status}}, Url: {{config.url}} Method: {{config.method}}')(response);
+        } else {
+            return angular.toJson(response);
+        }
+    };
+
+    return {
+        response: function (response) {
+            HttpLoggingService.returnToService(response.config.url);
+            return response || $q.when(response);
+        },
+        responseError: function (rejection) {
+            $log.error('ccHttpResponseInterceptor response error', rejection);
+            var logMessage = createLogMessage(rejection);
+            HttpLoggingService.sendLog(logMessage);
+
+            if (rejection.status === 403) {
+                $rootScope.$broadcast('ccAppError', {type: 'danger', msg: 'Access Denied'});
+            }
+            else if (rejection.status === 503) {
+                HttpLoggingService.serviceUnavailable(rejection.config.url, rejection.data.service);
+                $rootScope.$broadcast('ccAppError',
+                    {
+                        type: 'danger',
+                        msg: 'Some data is unavailable. If necessary, please refresh your page and try again.'
+                    });
+            }
+            else if (rejection.status === 0) {
+                //timeout or cancel? - they navigated away from the page
+                $log.error('request was canceled or timed out ' + rejection.config.url);
+            }
+            else {
+                $rootScope.$broadcast('ccAppError',
+                    {
+                        type: 'danger',
+                        msg: 'There was an error making a request. The support team has been notified. If necessary, please refresh your page and try again.'
+                    });
+            }
+            return $q.reject(rejection);
+        }
+    };
+});
+
 
 app.service('HttpLoggingService', function ($httpBackend, $window) {
     var buildLogInfo = function (msg) {
